e3a6ac22895afca00cb506729811ed04f84d4252
qin yang <466168951@qq.com>
2016-05-27 17:43:05 +0800

    修复eleManage ，适用于每种标签（之前傻逼了，只做了div标签的管理）

--- fe/eleManage.js
+++ fe/eleManage.js
@@ -1,29 +1,29 @@
 /*
 * @Author: apple
 * @Date:   2016-02-17 17:53:44
-* @Last Modified by:   apple
-* @Last Modified time: 2016-02-17 22:03:33
+* @Last Modified by:   qin yang
+* @Last Modified time: 2016-05-27 17:40:34
 */
 
 ;(function () {
   // 判断是否有某个class
-  HTMLDivElement.prototype.hasClass = function (cls) {
+  HTMLElement.prototype.hasClass = function (cls) {
     var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
     return reg.test(this.className);
   }
   // 添加一个class
-  HTMLDivElement.prototype.addClass = function (cls) {
+  HTMLElement.prototype.addClass = function (cls) {
     if (!this.hasClass(cls)) this.className += ' ' + cls;
   }
   // 删除某个class
-  HTMLDivElement.prototype.removeClass = function (cls) {
+  HTMLElement.prototype.removeClass = function (cls) {
     if (this.hasClass(cls)) {
       var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
       this.className = this.className.replace(reg, ' ');
     }
   }
   // 开关某个class
-  HTMLDivElement.prototype.toggleClass = function (cls) {
+  HTMLElement.prototype.toggleClass = function (cls) {
     if (this.hasClass(cls)) {
       this.removeClass(cls);
     } else {
@@ -32,22 +32,22 @@
   }
 
   // 隐藏某个元素
-  HTMLDivElement.prototype.hide = function () {
+  HTMLElement.prototype.hide = function () {
     this.style.display = 'none';
   }
 
   // 显示某个元素
-  HTMLDivElement.prototype.show = function () {
+  HTMLElement.prototype.show = function () {
     this.style.display = '';
   }
 
   // 查看元素是否是隐藏状态
-  HTMLDivElement.prototype.isHide = function () {
+  HTMLElement.prototype.isHide = function () {
     return this.style.display === 'none' ? true : false;
   }
 
   // 查看元素是否是显示状态
-  HTMLDivElement.prototype.isShow = function () {
+  HTMLElement.prototype.isShow = function () {
     return this.style.display !== 'none' ? true : false;
   }
 })();
