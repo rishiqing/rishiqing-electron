c391711fb4f059642979e248b4d7d1faf2748c03
qinyang <466168951@qq.com>
2017-11-27 12:18:22 +0800

    fix

--- dmg.js
+++ dmg.js
@@ -2,8 +2,8 @@
 /*
 * @Author: qin yang
 * @Date:   2016-11-18 16:21:58
-* @Last Modified by:   qin yang
-* @Last Modified time: 2016-11-18 20:51:39
+* @Last Modified by:   qinyang
+* @Last Modified time: 2017-11-20 21:52:02
 */
 
 var appdmg = require('appdmg');
@@ -11,8 +11,8 @@ var argv   = require('yargs').argv;
 var package = require('./package.json');
 var fs      = require('fs');
 var env    = argv.env ? argv.env : 'release';
-var target = `./dmg/日事清-${env}-osx-${package.version}.dmg`;
-var appPath = `./package/${env}-x64/日事清-darwin-x64/日事清.app`;
+var target = `./dmg/rishiqing-${env}-osx-${package.version}.dmg`;
+var appPath = `./package/${env}-x64/rishiqing-darwin-x64/rishiqing.app`;
 
 try {
   var outDirState = fs.lstatSync('dmg');
@@ -29,7 +29,7 @@ var dmg = appdmg({
 	basepath: __dirname, 
 	target: target,
 	specification: {
-		"title": "日事清",
+		"title": "rishiqing",
 		"icon": "./res/app.icns",
 		"contents": [
 			{ "x": 200, "y": 120, "type": "link", "path": "/Applications" },
