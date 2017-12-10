3377e2019021f57c30e932ca29f8c79bbbfdfd57
qinyang <466168951@qq.com>
2017-12-10 23:44:47 +0800

    fix: 改用electron-builder.js文件来进行打包，在afterPack回调里执行代码，在zh_CN.lproj下面添加InfoPlist.strings文件，这样包名可以是rishiqing.app，但是显示出来的是日事清【终于解决了这个困扰已久的问题】

---
+++ electron-builder.js
@@ -0,0 +1,64 @@
+const builder = require('electron-builder');
+const fs      = require('fs');
+const path    = require('path');
+
+// zh_CN.lproj里InfoPlist.strings
+const zh_CN = `
+/* Localized versions of Info.plist keys */
+
+"CFBundleDisplayName" = "日事清";
+"CFBundleName" = "日事清";
+`;
+
+builder.build({
+  config: {
+    appId: 'release.rishiqing.electron',
+    productName: 'rishiqing',
+    electronVersion: '1.7.9',
+    directories: {
+      output: 'package',
+      app: 'dir'
+    },
+    publish: {
+      provider: 'generic',
+      url: 'https://rishiqing-client.oss-cn-shenzhen.aliyuncs.com/pc-autoupdate/${os}/${env.CHANNEL}',
+      channel: '${env.CHANNEL}'
+    },
+    mac: {
+      category: 'public.app-category.productivity', //放到生产效率类
+      icon: 'res/app.icns',
+      identity: '72X2RKD3B2',
+      type: 'distribution'
+    },
+    dmg: {
+      artifactName: 'rishiqing-mac-${env.CHANNEL}-${version}.${ext}'
+    },
+    win: {
+      target: 'nsis',
+      icon: 'res/256x256.ico',
+      publish: {
+        provider: 'generic',
+        url: 'https://rishiqing-client.oss-cn-shenzhen.aliyuncs.com/pc-autoupdate/${os}/${env.CHANNEL}/${env.ARCH}',
+        channel: '${env.CHANNEL}'
+      }
+    },
+    nsis: {
+      artifactName: 'rishiqing-win-${env.ARCH}-${env.CHANNEL}-${version}.exe',
+      shortcutName: '日事清',
+      uninstallDisplayName: '${productName} ${version}',
+      guid: 'F4BC9A4A-E09B-465E-BC10-A8921C46E672'
+    },
+    afterPack: function (options) {
+      if (options.electronPlatformName !== 'darwin') return null;
+      // console.log('arguments', arguments);
+      // package/mac/rishiqing.app/Contents/Resources/zh_CN.lproj
+      return new Promise (function (resolve, reject) {
+        fs.writeFile(path.join(options.appOutDir, 'rishiqing.app/Contents/Resources/zh_CN.lproj/InfoPlist.strings'), zh_CN, (err) => {
+          console.log('write file', err);
+          if (err) reject(err);
+          else resolve();
+        });
+      });
+    }
+  }
+});
