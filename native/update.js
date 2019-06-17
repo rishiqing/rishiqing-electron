d3d86723d4be52896f2a9c093150d496374b3403
lumozx <969671691@qq.com>
2019-06-17 10:09:33 +0800

    剔除无用文件

--- native/update.js
+++ native/update.js
@@ -1,7 +1,3 @@
-const request = require('request');
-const config = require('./config');
-const BrowserWindow = require('electron').BrowserWindow;
-const nativeImage   = require('electron').nativeImage;
 const Notification = require('electron').Notification;
 const autoUpdater = require('electron-updater').autoUpdater;
 const pkg = require('../package.json');
@@ -11,32 +7,7 @@ class Update {
   constructor (mainWindow) {
     this.mainWindow = mainWindow;
     this.webContents = mainWindow.webContents;
-    // this.initUpdate();
     this.setAutoUpdate();
-  }
-  initUpdate () {
-    request.post({ 
-      url: config.VERSIONURL, 
-      headers: { 'User-Agent': this.webContents.getUserAgent() }
-    }, (err, res, body) => {
-      try {
-        const versionInfo = JSON.parse(body).versionInfo;
-        const version_code = versionInfo.version_code;
-        if (version_code > config.VERSION) { // 说明有更新
-          const win = new BrowserWindow({
-            width: 300,
-            height: 120,
-            alwaysOnTop: true,
-            resizable: false,
-            webPreferences: {
-              nodeIntegration: true
-            },
-            icon: nativeImage.createFromPath(__dirname + '/../res/icon_256x256.png')
-          });
-          win.loadURL('file://' + __dirname + '/../native/update/autoUpdate.html');
-        }
-      } catch (e) {  }
-    });
   }
   setAutoUpdate () {
     autoUpdater.on('update-downloaded', function (info) {
