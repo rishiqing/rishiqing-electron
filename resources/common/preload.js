b278ca0412dbfd7075b5bab626ef371e43ed7313
子潇 <969671691@qq.com>
2024-01-08 20:24:32 +0800

    fix: css

--- resources/common/preload.js
+++ resources/common/preload.js
@@ -1,10 +1,10 @@
-const ipc = require("electron").ipcRenderer;
-const package_json = require("../../package.json");
+const ipc = require('electron').ipcRenderer
+const package_json = require('../../package.json')
 const EVENTS = require('./preload_event')
 
 function dealLogin(canAutoLogin) {
   if (!canAutoLogin) {
-    ipc.sendToHost(EVENTS.Preload_Can_Auto_Login, canAutoLogin);
+    ipc.sendToHost(EVENTS.Preload_Can_Auto_Login, canAutoLogin)
   }
 }
 
@@ -12,29 +12,42 @@ window.onload = () => {
   //VERSIONSTAMP
   window.VERSIONSTAMP = {
     version: package_json.version,
-    time: package_json.releaseTime || new Date().toString()
-  };
-
+    time: package_json.releaseTime || new Date().toString(),
+  }
 
   //Client_Can_Auto_Login
-  let Client_Can_Auto_Login_Data = "";
+  let Client_Can_Auto_Login_Data = ''
   if (window.Client_Can_Auto_Login !== undefined) {
-    dealLogin(window.Client_Can_Auto_Login);
+    dealLogin(window.Client_Can_Auto_Login)
   }
-  Client_Can_Auto_Login_Data = window.Client_Can_Auto_Login_Data;
-  window.Object.defineProperty(window, "Client_Can_Auto_Login", {
+  Client_Can_Auto_Login_Data = window.Client_Can_Auto_Login_Data
+  window.Object.defineProperty(window, 'Client_Can_Auto_Login', {
     configurable: true,
     get() {
-      return Client_Can_Auto_Login_Data;
+      return Client_Can_Auto_Login_Data
     },
     set(v) {
-      Client_Can_Auto_Login_Data = v;
-      dealLogin(v);
-    }
-  });
+      Client_Can_Auto_Login_Data = v
+      dealLogin(v)
+    },
+  })
 
   // web端返回302,pc返回200
   window.onLogout = () => {
-    ipc.sendToHost(EVENTS.Preload_On_Logout);
-  };
-};
+    ipc.sendToHost(EVENTS.Preload_On_Logout)
+  }
+
+  // 解决图片使用vertical-align: baseline会撑出滚动条的问题
+  const cssContent = `
+  #r-login-register {
+    user-select: none
+  }
+  .r-login-register__sidebar-img {
+    vertical-align: bottom
+  }
+  `
+
+  const style = document.createElement('style')
+  style.innerHTML = cssContent
+  document.head.appendChild(style)
+}
